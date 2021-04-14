0b127337b17fcdd4e83674d00e1af7da340189d5
Aimarall <aimaral21072004@gmail.com>
2021-04-14 16:33:59 +0600

    added components to src

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import About from "./components/About/About";
+import Contacts from "./components/Contacts/Contacts";
+import Home from "./components/Home/Home";
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <About />
+      <Contacts />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
