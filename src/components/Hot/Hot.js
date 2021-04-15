04ab78375129a17d2e3a5a13d7939760d5ea6867
Aimarall <aimaral21072004@gmail.com>
2021-04-15 13:16:57 +0600

    added files to src

--- src/components/Hot/Hot.js
+++ src/components/Hot/Hot.js
@@ -1,11 +1,11 @@
-import classes from "./Kosmos.module.css";
+import classes from "./Hot.module.css";
 
 
-const Kosmos = ({ }) => {
+const Hot = ({ }) => {
 
   return (
-    <div className={classes.Kosmos}>
-      <h1>Kosmos</h1>
+    <div className={classes.Hot}>
+      <h1>Hot</h1>
       <p>
         The ocean (also the sea or the world ocean) is the body of salt water which covers approximately 71% of the surface of the Earth.[1] It is also "any of the large bodies of water into which the great ocean is divided".[1] These five oceans are, in descending order by area, the Pacific, Atlantic, Indian, Southern (Antarctic), and Arctic Oceans.[2][3]
         Saline seawater covers approximately 361,000,000 km2 (139,000,000 sq mi) and is customarily divided into several principal oceans and smaller seas, with the ocean as a whole covering approximately 71% of Earth's surface and 90% of the Earth's biosphere.[4] The oceans contains 97% of Earth's water, and oceanographers have stated that less than 20% of the oceans have been mapped.[4] The total volume is approximately 1.35 billion cubic kilometers (320 million cu mi) with an average depth of nearly 3,700 meters (12,100 ft).[5][6][7]
@@ -21,4 +21,4 @@ const Kosmos = ({ }) => {
   );
 }
 
-export default Kosmos;
+export default Hot;
