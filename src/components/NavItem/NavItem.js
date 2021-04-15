38f775e1e3d50217c752db76e2553fa0f932f131
Aimarall <aimaral21072004@gmail.com>
2021-04-15 15:19:57 +0600

    chnaged functions of the files

---
+++ src/components/NavItem/NavItem.js
@@ -0,0 +1,10 @@
+// import { Children } from "react"
+// import { Link, Navlink } from "react-router-dom"
+
+// const NavItem = ({ to, Children }) => {
+//     return (
+//         <li>
+//             <Navlink>hi</Navlink>
+//         </li>
+//     )
+// }
