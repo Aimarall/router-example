ec279804332ee26fc06006bfb482982035dd9763
Aimarall <aimaral21072004@gmail.com>
2021-04-15 13:29:53 +0600

    deleted text kosmos and changes to space word

--- src/components/Hot/Hot.js
+++ src/components/Hot/Hot.js
@@ -7,14 +7,58 @@ const Hot = ({ }) => {
     <div className={classes.Hot}>
       <h1>Hot</h1>
       <p>
-        The ocean (also the sea or the world ocean) is the body of salt water which covers approximately 71% of the surface of the Earth.[1] It is also "any of the large bodies of water into which the great ocean is divided".[1] These five oceans are, in descending order by area, the Pacific, Atlantic, Indian, Southern (Antarctic), and Arctic Oceans.[2][3]
-        Saline seawater covers approximately 361,000,000 km2 (139,000,000 sq mi) and is customarily divided into several principal oceans and smaller seas, with the ocean as a whole covering approximately 71% of Earth's surface and 90% of the Earth's biosphere.[4] The oceans contains 97% of Earth's water, and oceanographers have stated that less than 20% of the oceans have been mapped.[4] The total volume is approximately 1.35 billion cubic kilometers (320 million cu mi) with an average depth of nearly 3,700 meters (12,100 ft).[5][6][7]
-        As the world's ocean is the principal component of Earth's hydrosphere, it is integral to life, forms part of the carbon cycle, and influences climate and weather patterns. The ocean is the habitat of 230,000 known species, but because much of it is unexplored, the number of species in the ocean is much larger, possibly over two million.[8] The origin of Earth's oceans is unknown; oceans are thought to have formed in the Hadean eon and may have been the cause for the emergence of life.
-        There are numerous environmental issues for oceans which include for example marine pollution, overfishing, ocean acidification and other effects of climate change on oceans.
-        The phrases "the ocean" or "the sea" used without specification refer to the interconnected body of salt water covering the majority of the Earth's surface.[2][3] it includes the Atlantic, Pacific, Indian, Southern and Arctic Oceans.[9] As a general term, "the ocean" is mostly interchangeable with "the sea" in American English, but not in British English.[10] Strictly speaking, a sea is a body of water (generally a division of the world ocean) partly or fully enclosed by land.[11] The word "sea" can also be used for many specific, much smaller bodies of seawater, such as the North Sea or the Red Sea. There is no sharp distinction between seas and oceans, though generally seas
-        are smaller, and are often partly (as marginal seas) or wholly (as inland seas) bordered by land.[12]
-        The global, interconnected body of salt water is sometimes referred to as the "World Ocean" or global ocean.[13][14] The concept of a continuous body of water with relatively free interchange among its parts is of fundamental importance to oceanography.[15] The contemporary concept of the World Ocean was coined in the early 20th century by the Russian oceanographer Yuly Shokalsky to refer to the continuous ocean that covers and encircles most of Earth.[16] Plate tectonics, post-glacial rebound, and sea level rise continually change the coastline and structure of the world ocean. That said a
-        global ocean has existed in one form or another on Earth for eons.
+        Places
+        Hot District, a district of Chiang Mai province, Thailand
+        Hot, Hot a sub-district of Hot District, Thailand
+        Tha Kham, also known as Hot, a town in Hot District, Chiang Mai province, Thailand
+        Hot, Albania, a village in the Malësi e Madhe municipality, Shkodër County, Albania
+        Music
+        H.O.T. (Korean band) (High-Five of Teenagers), a Korean pop group
+        Hawaii Opera Theatre, an opera company located in Honolulu, Hawaii
+        Hot (American vocal group), best known for 1977 hit "Angel in Your Arms" 1976–1980
+        Hot 97, branding for hip-hop radio station WQHT in New York City
+        Albums
+        Hot (Paul Bley album), 1985
+        Hot (Squirrel Nut Zippers album), 1996
+        Hot (Melanie Brown album), 2000
+        Hot (Inna album), 2009
+        Hot (Freda Payne album), 1979
+        Hot (Half Japanese album), 1995
+        Extended plays
+        Hot (EP), a 2008 EP by Taeyang
+        Songs
+        "Hot" (Avril Lavigne song), 2007
+        "Hot" (Beatnuts song), 2004
+        "Hot" (Inna song), 2009
+        "Hot", a 2019 song by Parah Dice
+        "Hot" (Young Thug song), 2019
+        "Hot (I Need to Be Loved, Loved, Loved, Loved)", a 1975 song by James Brown
+        Transport
+        Ho Tin stop, in Hong Kong (MTR station code)
+        Memorial Field Airport, in Hot Springs, Arkansas, United States
+        Other uses
+        Pungency
+        Hot (Israel), an Israeli cable company
+        Highly optimized tolerance, a general framework for studying complexity
+        HOT (missile), an anti-tank missile system
+        Hawaii Ocean Time-series, an oceanographic time-series study
+        High-occupancy toll lane, a variable toll payable by some road users
+        A series of psychedelic phenethylamines:
+        HOT-2
+        HOT-7
+        HOT-17
+        Hawkes Ocean Technologies, a developer of deep sea equipment
+        Head-of-train device, a console for the end-of-train device
+        HOT Bowl (full name: C.H.A.M.P.S. Heart of Texas Bowl), a college football game
+        Higher-order thought, a concept in higher-order thought theories of consciousness
+        A slang term for a physically attractive person
+        See also
+        Heat
+        Hots (disambiguation)
+        Hot Hot Hot (disambiguation)
+        Hot zone (disambiguation)
+        Hotspot (disambiguation)
+        Hotbox (disambiguation)
      </p>
 
     </div>
