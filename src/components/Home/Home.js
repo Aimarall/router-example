0f6adc582b91436e824818b8815c31d46dcf9bec
Aimarall <aimaral21072004@gmail.com>
2021-04-15 12:37:58 +0600

    added to three ctr text

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,18 @@
 import classes from "./Home.module.css";
 
 
-const Home = ({  }) => {
-  
+const Home = ({ }) => {
+
   return (
     <div className={classes.Home}>
-     <h1>Home</h1> 
-     <p>Hello world</p>
-     
+      <h1>Home</h1>
+      <p>
+        I live in a house. I like my house very much. It’s neither big nor little. When you enter the house you see a big living room. There is a sofa and a round table in the middle of the living room. There is a big TV-set on the wall. My living room is light as there are 4 big windows. There is a kitchen to the right and stairs to the upper floor. The kitchen is big, and the whole family can get together for dinner there. My parents also like to invite their friends for dinner.
+        My room is upstairs. It’s big, with high ceilings. There is not much furniture there, only my bed, my wardrobe and a couple of shelves. I keep my pets in the room. I have many of them: mice, rats, hamsters, fish, parrots. I even have a ferret. So there are cages all over the room. My parents’ room, my elder sister’s room, my younger brother’s room and my granny’s room are also upstairs.
+        There is a yard around the house. My granny likes to grow flowers there. My dad made a small sports ground with a horizontal bar and a punch bag. There is a swing opposite the sports ground.
+        There is a garage for our car and my dad’s tools. We also keep our bikes there.
+     </p>
+
     </div>
   );
 }
