d10404f4fd74a4279f802613290f4b6afb784d5b
Aimarall <aimaral21072004@gmail.com>
2021-04-15 13:16:22 +0600

    added fonts family to text

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import { Route } from 'react-router';
 import './App.css';
+import { Link } from 'react-router-dom';
+
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Contacts from "./components/Contacts/Contacts";
-import { Link } from 'react-router-dom';
+import Water from "./components/Water/Water";
+import Sun from "./components/Sun/Sun";
+import Hot from "./components/Hot/Hot";
+import Could from "./components/Could/Could";
+import Mountain from "./components/Mountain/Mountain";
+import Earth from "./components/Earth/Earth";
+import Kosmos from "./components/Kosmos/Kosmos";
 
 
 
@@ -15,6 +23,13 @@ function App() {
         <Link to="/" >Home</Link>
         <Link to="/about" >About</Link>
         <Link to="/contacts" >Contacts</Link>
+        <Link to="/water" >Water</Link>
+        <Link to="/sun" >Sun</Link>
+        <Link to="/hot" >Hot</Link>
+        <Link to="/could" >Could</Link>
+        <Link to="/mountain" >Mountain</Link>
+        <Link to="/earth" >Earth</Link>
+        <Link to="/kosmos" >Kosmos</Link>
       </div>
 
 
@@ -22,6 +37,13 @@ function App() {
       <Route path="/" component={Home} exact />
       <Route path="/about" component={About} />
       <Route path="/contacts" component={Contacts} />
+      <Route path="/water" component={Water} />
+      <Route path="/sun" component={Sun} />
+      <Route path="/hot" component={Hot} />
+      <Route path="/could" component={Could} />
+      <Route path="/mountain" component={Mountain} />
+      <Route path="/earth" component={Earth} />
+      <Route path="/kosmos" component={Kosmos} />
 
     </div>
   );
